1203f4764306a55c49e033a4dcdc55cb4ea07003
Pavel Amelkov <stelss78@gmail.com>
2025-08-24 11:18:48 +0300

    Added --check-packages. Supported comments and whitespaces in recipe

--- src/main.zig
+++ src/main.zig
@@ -3,39 +3,152 @@ const SimpleArena = @import("utils/simple_arena.zig").SimpleArena;
 
 var arena = SimpleArena(1048576).init();
 
-fn checkPackages(stdout: anytype, packageManager: *const PackageManager) !void {
+const PackageDiff = struct
+{
+    toRemove: []const []const u8,
+    toAdd: []const []const u8
+};
+
+fn stringCompare(_: void, lhs: []const u8, rhs: []const u8) bool
+{
+    return std.mem.lessThan(u8, lhs, rhs);
+}
+
+fn packageDiff(installed: []const []const u8, in_recipe: []const []const u8) !PackageDiff
+{
+    // TODO: two array lists work poorly with one arena.
+    // Ideally, you would like to have separate arenas for them
+    var to_add = std.ArrayList([]const u8).init(arena.allocator());
+    var to_remove = std.ArrayList([]const u8).init(arena.allocator());
+
+    var installed_index: usize = 0;
+    var recipe_index: usize = 0;
+
+    while (true)
+    {
+        if (installed_index == installed.len)
+        {
+            while (recipe_index < in_recipe.len)
+            {
+                try to_add.append(in_recipe[recipe_index]);
+                recipe_index += 1;
+            }
+            break;
+        }
+        if (recipe_index == in_recipe.len)
+        {
+            while (installed_index < installed.len)
+            {
+                try to_remove.append(installed[installed_index]);
+                installed_index += 1;
+            }
+        }
+
+        switch (std.mem.order(u8, installed[installed_index], in_recipe[recipe_index]))
+        {
+            .lt => {
+                try to_remove.append(installed[installed_index]);
+                installed_index += 1;
+            },
+            .eq => {
+                installed_index += 1;
+                recipe_index += 1;
+            },
+            .gt => {
+                try to_add.append(in_recipe[recipe_index]);
+                recipe_index += 1;
+            }
+        }
+        // installed[installed_index]
+    }
+
+    return PackageDiff{.toAdd = to_add.items, .toRemove = to_remove.items };
+}
+
+fn parcePackages(packages: *std.ArrayList([]const u8), string_with_packages: []const u8) !void
+{
+    var start_index: usize = 0;
+
+    while (std.mem.indexOfScalarPos(u8, string_with_packages, start_index, '\n')) |end_index|
+    {
+        if (std.mem.indexOfNone(u8, string_with_packages[start_index..end_index], &std.ascii.whitespace) != null and
+            string_with_packages[start_index] != '#')
+        {
+            try packages.append(string_with_packages[start_index..end_index]);
+        }
+        start_index = end_index + 1;
+    }
+}
+
+fn gatherPackages(pmfolder: std.fs.Dir) ![][]const u8
+{
     const allocator = arena.allocator();
+    var dir_iter = pmfolder.iterateAssumeFirstIteration();
+
+    var packages = std.ArrayList([]const u8).init(allocator);
+
+    while (try dir_iter.next()) |file_entry|
+    {
+        if (file_entry.kind != std.fs.File.Kind.file or
+            !std.mem.endsWith(u8, file_entry.name, "packages"))
+        {
+            continue;
+        }
+
+        var file = try pmfolder.openFile(file_entry.name, std.fs.File.OpenFlags{.lock = .shared});
+        const file_content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
+
+        try parcePackages(&packages, file_content);
+    }
+
+    std.mem.sortUnstable([]const u8, packages.items, {}, stringCompare);
+
+    return packages.items;
+}
+
+fn getInstalledPackages(packageManager: *const PackageManager) ![][]const u8{
+    const allocator = arena.allocator();
+
+    var packages = std.ArrayList([]const u8).init(allocator);
 
     const result = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = packageManager.userInstalledPackagesCmd,
+        .argv = packageManager.user_installed_cmd,
     });
 
-    try stdout.print("{s}", .{result.stdout});
+    try parcePackages(&packages, result.stdout);
+    std.mem.sortUnstable([]const u8, packages.items, {}, stringCompare);
+
+    return packages.items;
 }
 
 const PackageManager = struct
 {
     name: []const u8,
-    userInstalledPackagesCmd: []const []const u8,
+    user_installed_cmd: []const []const u8,
+    // cleanup
+    // install command
+    // remove command
     // allInstalledPackagesCmd: [][]const u8,
 };
 
 const package_managers: [1]PackageManager = .{
     .{
         .name = "dnf",
-        .userInstalledPackagesCmd = &.{"dnf", "repoquery", "--userinstalled", "--queryformat=%{name}\n"}
+        .user_installed_cmd = &.{"dnf", "repoquery", "--userinstalled", "--queryformat=%{name}\n"}
     },
 };
 
-fn getPackageManager(packageDir: std.fs.Dir.Entry) ?*const PackageManager
+fn getPackageManager(packageDir: std.fs.Dir.Entry) ?u63
 {
-    for (package_managers) |manager|
+    var index: u63 = 0;
+    while (index < package_managers.len)
     {
-        if (std.mem.eql(u8, packageDir.name, manager.name))
+        if (std.mem.eql(u8, packageDir.name, package_managers[index].name))
         {
-            return &manager;
+            return index;
         }
+        index += 0;
     }
     return null;
 }
@@ -48,7 +161,7 @@ pub fn main() !void {
 
     defer stdout.context.flush() catch {};
 
-    if (std.os.argv.len < 2)
+    if (std.os.argv.len < 3)
     {
         try stdout.print("Too little command line arguments\n", .{});
         return;
@@ -64,19 +177,40 @@ pub fn main() !void {
 
     var dir_iter = recipyDir.iterateAssumeFirstIteration();
 
-    while (try dir_iter.next()) |child_dir|
+    if (std.mem.orderZ(u8, std.os.argv[2], "--check-packages") == .eq)
     {
-        if (child_dir.kind != std.fs.File.Kind.directory)
+        while (try dir_iter.next()) |child_dir|
         {
-            continue;
-        }
+            if (child_dir.kind != std.fs.File.Kind.directory)
+            {
+                continue;
+            }
 
-        if (getPackageManager(child_dir)) |manager|
-        {
-            try checkPackages(stdout, manager);
+            if (getPackageManager(child_dir)) |manager_index|
+            {
+                const manager = &package_managers[manager_index];
+                const packages = try gatherPackages(
+                    try recipyDir.openDir(child_dir.name, .{.access_sub_paths = true, .iterate = true, .no_follow = true}));
+
+                const installed = try getInstalledPackages(manager);
+
+                const diff = try packageDiff(installed, packages);
+
+                try stdout.print("Packages missing:\n\n", .{});
+                for (diff.toAdd) |toAdd|
+                {
+                    try stdout.print("{s}\n", .{toAdd});
+                }
+
+                try stdout.print("\nExcessive packages:\n\n", .{});
+                for (diff.toRemove) |toRemove|
+                {
+                    try stdout.print("{s}\n", .{toRemove});
+                }
+            }
         }
-        try stdout.print("{s}\n", .{child_dir.name});
     }
+
 
     try stdout.context.flush();
 }
